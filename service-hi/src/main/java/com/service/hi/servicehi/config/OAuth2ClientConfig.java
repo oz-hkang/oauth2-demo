9e7f09fe4f4cce33bcfd3b3ff56877ae205c897f
Thomas Kang <thomas.kang@hrx.ai>
2019-04-13 16:37:08 +0800

    fix

--- service-hi/src/main/java/com/service/hi/servicehi/config/OAuth2ClientConfig.java
+++ service-hi/src/main/java/com/service/hi/servicehi/config/OAuth2ClientConfig.java
@@ -1,11 +1,14 @@
 package com.service.hi.servicehi.config;
 
-import feign.RequestInterceptor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
@@ -13,34 +16,61 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
+import org.springframework.web.client.RestTemplate;
 
-@EnableOAuth2Client
-@EnableConfigurationProperties
-@Configuration
-public class OAuth2ClientConfig {
+import feign.RequestInterceptor;
 
-//    @Bean
-//    @ConfigurationProperties(prefix = "security.oauth2.client")
-//    public ClientCredentialsResourceDetails clientCredentialsResourceDetails() {
-//        return new ClientCredentialsResourceDetails();
-//    }
+
+/**
+ * TODO: DOCUMENT ME!
+ *
+ * @author   <a href="mailto:thomas.kang@hrx.ai">Thomas</a>
+ * @version  04/13/2019 16:34
+ */
+@Configuration @EnableConfigurationProperties @EnableOAuth2Client public class OAuth2ClientConfig {
+  //~ Methods ----------------------------------------------------------------------------------------------------------
+
+  /**
+   * clientCredentialsResourceDetails.
+   *
+   * @return  ClientCredentialsResourceDetails
+   */
+  @Bean
+  @ConfigurationProperties(prefix = "security.oauth2.client")
+  public ClientCredentialsResourceDetails clientCredentialsResourceDetails() {
+    return new ClientCredentialsResourceDetails();
+  }
+
+  //~ ------------------------------------------------------------------------------------------------------------------
+
+// @Bean
+// public RequestInterceptor oauth2FeignRequestInterceptor() {
+// return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), clientCredentialsResourceDetails());
+// }
 //
-//    @Bean
-//    public RequestInterceptor oauth2FeignRequestInterceptor() {
-//        return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), clientCredentialsResourceDetails());
-//    }
-//
-//    @Bean
-//    public OAuth2RestTemplate clientCredentialsRestTemplate() {
-//        return new OAuth2RestTemplate(clientCredentialsResourceDetails());
-//    }
+  /**
+   * restTemplate.
+   *
+   * @return  RestTemplate
+   */
+  @Bean @LoadBalanced public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 
-    @Bean
-    public ResourceServerTokenServices tokenService() {
+  //~ ------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * tokenService.
+   *
+   * @return  ResourceServerTokenServices
+   */
+  @Bean public ResourceServerTokenServices tokenService() {
     RemoteTokenServices tokenServices = new RemoteTokenServices();
-        tokenServices.setClientId("client_2");
-        tokenServices.setClientSecret("123456");
-        tokenServices.setCheckTokenEndpointUrl("http://localhost:9098/oauth/check_token");
-        return tokenServices;
-    }
-}
+    tokenServices.setRestTemplate(restTemplate());
+    tokenServices.setClientId("client_2");
+    tokenServices.setClientSecret("123456");
+    tokenServices.setCheckTokenEndpointUrl("http://service-auth/oauth/check_token");
+
+    return tokenServices;
+  }
+} // end class OAuth2ClientConfig
