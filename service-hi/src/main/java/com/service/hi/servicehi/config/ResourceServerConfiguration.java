22456aa8ece6855003897525f27f5e6e22127e06
Thomas Kang <thomas.kang@hrx.ai>
2019-04-13 14:03:40 +0800

    change

--- service-hi/src/main/java/com/service/hi/servicehi/config/ResourceServerConfiguration.java
+++ service-hi/src/main/java/com/service/hi/servicehi/config/ResourceServerConfiguration.java
@@ -1,21 +1,30 @@
 package com.service.hi.servicehi.config;
 
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
+
+/**
+ * TODO: DOCUMENT ME!
+ *
+ * @author   <a href="mailto:thomas.kang@hrx.ai">Thomas</a>
+ * @version  04/13/2019 11:23
+ */
 @Configuration
-@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+@EnableResourceServer public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+  //~ Methods ----------------------------------------------------------------------------------------------------------
 
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/order/**").authenticated(); // 配置order访问控制，必须认证后才可以访问
-    }
+  /**
+   * @see  org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
+   */
+  @Override public void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/order/**").authenticated().antMatchers("/scope/**/").access(
+      "#oauth2.hasScope('read')").antMatchers("/role/**/").access("#oauth2.hasRole('ROLE_USER')"); // 配置order访问控制，必须认证后才可以访问
+  }
 }
--- service-hi/src/main/java/com/service/hi/servicehi/controller/TestEndPointController.java
+++ service-hi/src/main/java/com/service/hi/servicehi/controller/TestEndPointController.java
@@ -1,54 +1,136 @@
 package com.service.hi.servicehi.controller;
 
-import com.service.hi.servicehi.dto.UserService;
-import com.service.hi.servicehi.entity.User;
+import java.security.Principal;
+
 import org.apache.commons.lang.StringUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
+import com.service.hi.servicehi.dto.UserService;
+import com.service.hi.servicehi.entity.User;
 
-@RestController
-public class TestEndPointController {
 
-    Logger logger = LoggerFactory.getLogger(TestEndPointController.class);
+/**
+ * TODO: DOCUMENT ME!
+ *
+ * @author   <a href="mailto:thomas.kang@hrx.ai">Thomas</a>
+ * @version  04/13/2019 11:22
+ */
+@RestController public class TestEndPointController {
+  //~ Instance fields --------------------------------------------------------------------------------------------------
 
-    @Autowired
-    private UserService userService;
+  /** TODO: DOCUMENT ME! */
+  Logger logger = LoggerFactory.getLogger(TestEndPointController.class);
 
-    @GetMapping("/product/{id}")
-    public String getProduct(@PathVariable String id) {
-        return "product id : " + id;
+  @Autowired private UserService userService;
+
+  //~ Methods ----------------------------------------------------------------------------------------------------------
+
+  /**
+   * createUser.
+   *
+   * @param   username  String
+   * @param   password  String
+   *
+   * @return  User
+   */
+  @RequestMapping(
+    value  = "/registry",
+    method = RequestMethod.POST
+  )
+  public User createUser(@RequestParam("username") String username,
+    @RequestParam("password") String password) {
+    if (StringUtils.isNotEmpty(username) && StringUtils.isNotEmpty(password)) {
+      return userService.create(username, password);
     }
 
-    @GetMapping("/order/{id}")
-    public String getOrder(@PathVariable String id) {
-        return "order id : " + id;
-    }
+    return null;
+  }
 
-    @GetMapping("/getPrinciple")
-    public OAuth2Authentication getPrinciple(OAuth2Authentication oAuth2Authentication, Principal principal, Authentication authentication) {
-        logger.info(oAuth2Authentication.getUserAuthentication().getAuthorities().toString());
-        logger.info(oAuth2Authentication.toString());
-        logger.info("principal.toString() " + principal.toString());
-        logger.info("principal.getName() " + principal.getName());
-        logger.info("authentication: " + authentication.getAuthorities().toString());
+  //~ ------------------------------------------------------------------------------------------------------------------
 
-        return oAuth2Authentication;
-    }
+  /**
+   * getter method for order.
+   *
+   * @param   id  String
+   *
+   * @return  String
+   */
+  @GetMapping("/order/{id}")
+  public String getOrder(@PathVariable String id) {
+    return "order id : " + id;
+  }
 
-    @RequestMapping(value = "/registry", method = RequestMethod.POST)
-    public User createUser(@RequestParam("username") String username, @RequestParam("password") String password) {
-        if (StringUtils.isNotEmpty(username) && StringUtils.isNotEmpty(password)) {
-            return userService.create(username, password);
-        }
+  //~ ------------------------------------------------------------------------------------------------------------------
 
-        return null;
-    }
-}
+  /**
+   * getter method for principle.
+   *
+   * @param   oAuth2Authentication  OAuth2Authentication
+   * @param   principal             Principal
+   * @param   authentication        Authentication
+   *
+   * @return  OAuth2Authentication
+   */
+  @GetMapping("/getPrinciple")
+  public OAuth2Authentication getPrinciple(OAuth2Authentication oAuth2Authentication, Principal principal,
+    Authentication authentication) {
+    logger.info(oAuth2Authentication.getUserAuthentication().getAuthorities().toString());
+    logger.info(oAuth2Authentication.toString());
+    logger.info("principal.toString() " + principal.toString());
+    logger.info("principal.getName() " + principal.getName());
+    logger.info("authentication: " + authentication.getAuthorities().toString());
+
+    return oAuth2Authentication;
+  }
+
+  //~ ------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * getter method for product.
+   *
+   * @param   id  String
+   *
+   * @return  String
+   */
+  @GetMapping("/product/{id}")
+  public String getProduct(@PathVariable String id) {
+    return "product id : " + id;
+  }
+
+  //~ ------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * testRole.
+   *
+   * @return  String
+   */
+  @GetMapping("/role")
+  public String testRole() {
+    return "Hello role";
+  }
+
+  //~ ------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * testScope.
+   *
+   * @return  String
+   */
+  @GetMapping("/scope")
+  public String testScope() {
+    return "Hello scope ";
+  }
+
+} // end class TestEndPointController
